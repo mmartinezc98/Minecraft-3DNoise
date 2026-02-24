2e9517a4776153f852cfdf60abd25ce975efab8a
mmartinezc98 <mariomartinezcarrillo98@gmail.com>
2026-02-24 10:47:13 +0100

    Creado coloreado del mapa por alturas

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [SerializeField] public GameObject cubePrefab;
+    [Header("Prefabs de cubos con materiales")]
+    [SerializeField] public GameObject waterPrefab;
+    [SerializeField] public GameObject sandPrefab;
+    [SerializeField] public GameObject grassPrefab;
+    [SerializeField] public GameObject dirtPrefab;
+    [SerializeField] public GameObject stonePrefab;
+    [SerializeField] public GameObject snowPrefab;
+
+    [Header("Nivel del agua")]
+    public int waterLevel = 3;   // Nivel global del agua
+
+
+
 
     [SerializeField] public int Width;
     [SerializeField] public int Height;
@@ -28,9 +40,32 @@ public class MapGenerator : MonoBehaviour
 
                 for (int y = 0; y < Height; y++)
                 {
-                    Instantiate(cubePrefab, new Vector3(x,y,z), Quaternion.identity);
+                    GameObject prefabToUse;
+
+
+                    // Selección del bloque según altura
+                    if (y < 3)
+                        prefabToUse = sandPrefab;          //arena
+                    else if (y < 6)
+                        prefabToUse = grassPrefab;         //cesped
+                    else if (y < 10)
+                        prefabToUse = dirtPrefab;          //tierra
+                    else if (y < 16)
+                        prefabToUse = stonePrefab;         //piedra
+                    else
+                        prefabToUse = snowPrefab;          //nieve
+
+                    Instantiate(prefabToUse, new Vector3(x, y, z), Quaternion.identity);
                 }
+
+                // Generamos el agua
+                if (Height < waterLevel)
+                {
+                    Instantiate(waterPrefab, new Vector3(x, waterLevel, z), Quaternion.identity);
+                }
+
             }
         }
+        }
     }
-}
+
